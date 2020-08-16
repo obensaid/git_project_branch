7a8ef7babb62107f59d46728d5dd553419c2ac63
obensaid <obenyilmaz@gmail.com>
2020-08-16 18:38:59 +0200

    feature dan commit

--- src/git_project_branch/BranchDeneme.java
+++ src/git_project_branch/BranchDeneme.java
@@ -5,6 +5,7 @@ public class BranchDeneme {
 	public static void main(String[] args) {
 
 		System.out.println("18:28 - master branchten ilk commit");
+		System.out.println("18:37 - feature branchten ilk commit");
 		
 	}
 
